d943a8237679c84f0dae4d90331996278309d7fb
likun <likun01@vcredit.com>
2017-12-15 16:20:44 +0800

    DASD

---
+++ app/src/main/java/com/ashlikun/ijkplay/simple/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ashlikun.ijkplay.simple;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<String> listDatas = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
